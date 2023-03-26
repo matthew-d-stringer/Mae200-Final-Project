942818fd1ab93ab445c5c50cd93cf14550133d8f
mdsflyboy <mdsflyboy@gmail.com>
2023-03-25 21:58:43 -0700

    Adjusting plotting for step 1

--- Matlab Code/Dual_Pendulum_Input.m
+++ Matlab Code/Dual_Pendulum_Input.m
@@ -4,12 +4,19 @@ u_k = zeros(T/0.01+1,1);
 
 t = 0:0.01:T;
 
-for i = 1:3
-    [u_k x_k] = NR_Dual_Pendulum(T, u_k);
-end
+% for i = 1:3
+%     [u_k x_k] = NR_Dual_Pendulum(T, u_k);
+% end
 close all
 hold on
 plot(t, x_k(1,:))
 plot(t, x_k(2,:))
 plot(t, x_k(3,:))
 hold off
+
+title("Plot of Positional States with Optimal Initial Input")
+lgd = legend({'$x(t)$', '$\theta_1(t)$', '$\theta_2(t)$'}, 'Interpreter','latex')
+lgd.Location = 'northwest';
+
+save initialInput.mat u_k x_k;
+saveas(gcf, "Step1_final_plot.png");
